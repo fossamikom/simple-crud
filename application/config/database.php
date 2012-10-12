6891786cfd8b376c38cc8588f073742200939cee
labs-0x99 <newbie.masih@hotmail.com>
2012-10-12 16:07:00 +0700

    initial commit

--- application/config/database.php
+++ application/config/database.php
@@ -50,8 +50,8 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'kebumen';
-$db['default']['database'] = 'ci';
+$db['default']['password'] = 'labs-0x99';
+$db['default']['database'] = 'ci_db1';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
